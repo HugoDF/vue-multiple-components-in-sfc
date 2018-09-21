e2c24a81980ed65dcfd1a85412f3de9468062da3
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2018-09-21 12:36:25 +0100

    Add JSX example

---
+++ 04-jsx/README.md
@@ -0,0 +1,20 @@
+# 04-jsx
+
+> Example writing multiple Vue components in a single file using JSX.
+
+See [App.vue](./src/App.vue).
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
---
+++ 04-jsx/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+<script>
+// inline component with JSX
+const HelloWorld = {
+  props: ['msg'],
+  render() {
+    return (<div>
+      <h1>Hello world</h1>
+      <div>{this.msg}</div>
+    </div>);
+  } 
+};
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
