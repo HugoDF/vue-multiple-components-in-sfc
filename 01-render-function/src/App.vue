4ab62cecfd5cab3d71bfbd435ad9805a8ec666d2
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2019-07-28 16:50:25 +0100

    fix render function example

---
+++ 01-render-function/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+<script>
+// inline component
+const HelloWorld = {
+  props: ['msg'],
+  render(h) {
+    return h('div', [
+      h('h1', 'Hello world'),
+      h('div', this.msg)
+    ])
+  }
+};
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
